46316ce0f0732a9fba7454aafb4942f9e6fa7fb0
Ilja Rotar <kontakt@ilja-rotar.de>
2025-05-07 10:11:22 +0200

    mgmt interfaces plural

--- configdb/configdb.go
+++ configdb/configdb.go
@@ -24,7 +24,7 @@ type ConfigDB struct {
 	MCLAGDomains       map[string]MCLAGDomain    `json:"MCLAG_DOMAIN,omitempty"`
 	MCLAGInterfaces    map[string]MCLAGInterface `json:"MCLAG_INTERFACE,omitempty"`
 	MCLAGUniqueIPs     map[string]MCLAGUniqueIP  `json:"MCLAG_UNIQUE_IP,omitempty"`
-	MgmtInterface      map[string]MgmtInterface  `json:"MGMT_INTERFACE,omitempty"`
+	MgmtInterfaces     map[string]MgmtInterface  `json:"MGMT_INTERFACE,omitempty"`
 	MgmtPorts          map[string]MgmtPort       `json:"MGMT_PORT,omitempty"`
 	MgmtVRFConfig      MgmtVRFConfig             `json:"MGMT_VRF_CONFIG"`
 	NTP                NTP                       `json:"NTP"`
@@ -73,7 +73,7 @@ func GenerateConfigDB(input *values.Values, platform *p.Platform, currentDeviceM
 		MCLAGDomains:    getMCLAGDomains(input.MCLAG),
 		MCLAGInterfaces: getMCLAGInterfaces(input.MCLAG),
 		MCLAGUniqueIPs:  getMCLAGUniqueIPs(input.MCLAG),
-		MgmtInterface:   getMgmtInterface(input.MgmtInterface),
+		MgmtInterfaces:  getMgmtInterfaces(input.MgmtInterface),
 		MgmtPorts: map[string]MgmtPort{
 			"eth0": {
 				AdminStatus: AdminStatusUp,
@@ -308,18 +308,16 @@ func getMCLAGUniqueIPs(mclag values.MCLAG) map[string]MCLAGUniqueIP {
 	}
 }
 
-func getMgmtInterface(mgmtif values.MgmtInterface) map[string]MgmtInterface {
+func getMgmtInterfaces(mgmtif values.MgmtInterface) map[string]MgmtInterface {
 	if mgmtif.IP == "" {
 		return nil
 	}
-
 	mgmtInterfaces := make(map[string]MgmtInterface)
 
 	eth0 := MgmtInterface{}
 	if mgmtif.GatewayAddress != "" {
 		eth0.GWAddr = mgmtif.GatewayAddress
 	}
-
 	mgmtInterfaces["eth0|"+mgmtif.IP] = eth0
 
 	return mgmtInterfaces
